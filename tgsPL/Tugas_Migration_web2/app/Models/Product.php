089934cdbe993329cd3fadea93ca6cd70cb57642
M Zherylda FSK <mzheryldafsk@gmail.com>
2026-04-03 21:20:24 +0700

    Add files via upload

---
+++ tgsPL/Tugas_Migration_web2/app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'stock', 'category_id', 'supplier_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
